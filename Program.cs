4a5c2d490654a40d3230b53f98c1ab36897f8075
ofcdougls <ofc.dougls@outlook.com>
2023-04-19 21:30:03 -0300

    repositorio completo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VCgostaDEpipoca
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("você gosta de pipoca?");
+        }
+    }
+}
